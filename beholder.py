Fix variance calculations. Before, variance was based off of already scaled windows.

beholder.py
@@ -1,6 +1,7 @@
 from collections import deque
 import json
 
+import cv2
 import numpy as np
 import tensorflow as tf
 
@@ -46,7 +47,7 @@ class Beholder():
     self.frames_over_time = deque([], DEFAULT_CONFIG['window_size'])
     self.frame_placeholder = None
     self.summary_op = None
-    self.old_values = None
+    self.old_config = None
 
 
   @staticmethod
@@ -65,11 +66,9 @@ class Beholder():
     return grads, optimizer.apply_gradients(grads_and_vars)
 
 
-
   def _get_config(self):
     try:
       json_string = pau.RetrieveAsset(self.LOGDIR_ROOT, PLUGIN_NAME, 'config')
-      print('json_string', json_string)
       return json.loads(json_string)
     except (KeyError, ValueError):
       print('Could not read config file. Creating a config file.')
@@ -84,45 +83,47 @@ class Beholder():
   def _get_display_frame(self, config, arrays=None):
     values, mode, scaling = config['values'], config['mode'], config['scaling']
 
-    if values != self.old_values:
+    if config != self.old_config:
       self.frames_over_time.clear()
-
-    self.old_values = values
+    self.old_config = config
 
     if values != CUSTOM:
       arrays = [self.SESSION.run(x) for x in tf.trainable_variables()]
 
-    global_min, global_max = im_util.global_extrema(arrays)
-    absolute_frame = im_util.arrays_to_image(arrays, scaling,
-                                             IMAGE_HEIGHT, IMAGE_WIDTH)
-
-    self.frames_over_time.append(absolute_frame)
+    columns = im_util.arrays_to_columns(arrays, IMAGE_HEIGHT, IMAGE_WIDTH)
+    self.frames_over_time.append(columns)
 
     if mode == CURRENT:
-      final_frame = absolute_frame
-
+      scaled_columns = im_util.scale_for_display(columns, scaling)
     elif mode == VARIANCE:
-      stacked = np.dstack(self.frames_over_time)
-      variance = np.var(stacked, axis=2)
-      final_frame = im_util.scale_for_display(variance, scaling,
-                                              global_min, global_max)
+      variance_columns = []
 
-    return final_frame
+      for i in range(len(columns)):
+        variance = np.var([columns[i] for columns in self.frames_over_time],
+                          axis=0)
+        variance_columns.append(variance)
+
+      scaled_columns = im_util.scale_for_display(variance_columns, scaling)
+
+    return cv2.resize(np.hstack(scaled_columns).astype(np.uint8),
+                      (IMAGE_WIDTH, IMAGE_HEIGHT),
+                      interpolation=cv2.INTER_NEAREST)
 
 
   def _write_summary(self, frame):
     summary = self.SESSION.run(self.summary_op, feed_dict={
         self.frame_placeholder: frame
     })
-
     path = '{}/{}'.format(self.PLUGIN_LOGDIR, SUMMARY_FILENAME)
 
     with open(path, 'wb') as file:
       file.write(summary)
 
+
   def _update_deque(self, window_size):
     if window_size != self.frames_over_time.maxlen:
       self.frames_over_time = deque(self.frames_over_time, window_size)
+
 
   def update(self, arrays=None, frame=None):
     config = self._get_config()

image_util.py
@@ -3,52 +3,57 @@ from math import ceil, floor, sqrt
 import cv2
 import numpy as np
 
+
 def global_extrema(arrays):
   return min([x.min() for x in arrays]), max([x.max() for x in arrays])
 
-def arrays_to_image(arrays, scaling_scope, image_height, image_width):
-  global_min, global_max = global_extrema(arrays)
+
+def arrays_to_columns(arrays, image_height, image_width):
+  '''
+  Input: unprocessed numpy arrays.
+  Returns: columns of the size that they will appear in the image, not scaled
+           for display. That needs to wait until after variance is computed.
+  '''
   column_width = (image_width / len(arrays))
 
-  def reshape_array(array):
-    array = np.ravel(array)
+  columns = []
 
-    element_count = np.prod(array.shape)
-    columns = int(ceil(sqrt((column_width * element_count) / image_height)))
-    rows = int(floor(element_count / columns))
+  for array in arrays:
+    flattened_array = np.ravel(array)
+
+    element_count = np.prod(flattened_array.shape)
+    col_count = int(ceil(sqrt((column_width * element_count) / image_height)))
+    row_count = int(floor(element_count / col_count))
 
     # Truncate whatever remaining values there are that don't fit. Hopefully,
     # it doesn't matter that the last few (< column count) aren't there.
-    return np.reshape(array[:rows * columns], (rows, columns))
+    columns.append(np.reshape(flattened_array[:row_count * col_count],
+                              (row_count, col_count)))
 
-  reshaped_arrays = [reshape_array(array) for array in arrays]
-
-  image_scaled_arrays = [scale_for_display(array,
-                                           scaling_scope,
-                                           global_min,
-                                           global_max)
-                         for array in reshaped_arrays]
-
-  final_arrays = [cv2.resize(array,
-                             (column_width, image_height),
-                             interpolation=cv2.INTER_NEAREST)
-                  for array in image_scaled_arrays]
-
-  # Fixes little off-by-one errors after things are concatenated.
-  return cv2.resize(np.hstack(final_arrays).astype(np.uint8),
-                    (image_width, image_height),
-                    interpolation=cv2.INTER_NEAREST)
+  return [cv2.resize(column,
+                     (column_width, image_height),
+                     interpolation=cv2.INTER_NEAREST)
+          for column in columns]
 
 
-def scale_for_display(array, scaling_scope, global_min, global_max):
+def scale_for_display(columns, scaling_scope):
+  '''
+  Input: unscaled columns.
+  Returns: columns scaled to [0, 255]
+  '''
+
+  new_columns = []
 
   if scaling_scope == 'layer':
-    minimum = array.min()
-    maximum = (array - minimum).max()
+    for column in columns:
+      column -= column.min()
+      new_columns.append(column * (255 / column.max()))
 
   elif scaling_scope == 'network':
-    minimum = global_min
-    maximum = global_max
+    global_min, global_max = global_extrema(columns)
 
-  array -= minimum
-  return array * (255 / maximum)
+    for column in columns:
+      column -= global_min
+      new_columns.append(column * (255 / global_max))
+
+  return new_columns