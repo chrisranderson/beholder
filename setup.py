ceefdba88595e3f91756d1db76c9eed25e6ca577
Chris Anderson <chris.anderson@byu.net>
2017-08-21 14:54:14 -0600

    Change setup.py to include resources directory as package data.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='beholder',
     version='0.0.1',
     packages=['beholder'],
+    package_data={'beholder': ['resources/*']},
     url='https://github.com/chrisranderson/beholder',
     author='Chris Anderson',
     install_requires=['pillow'],
